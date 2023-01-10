a36c0873b6ad183741765f05b9278e2fb32d3910
davidrusyki <davidrusyck@gmail.com>
2023-01-10 09:11:44 -0300

    findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,9 +13,15 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("=== Test 1: seller findById ===");
-		Seller test = sellerDao.findById(3);		
-		
+		Seller test = sellerDao.findById(3);
 		System.out.println(test);
+		
+		System.out.println("=== Test @: seller findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> listFromDepartment = sellerDao.findByDepartment(department);
+		for (Seller seller : listFromDepartment) {
+			System.out.println(seller);
+		}
 		
 	}
 	
